cbaed868cf3fcec845ee6a4d039351c7d3c5618e
Wojciech Paluch <wojciech.paluch@o2.pl>
2017-08-12 14:11:58 +0200

    Added scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var myFirstVar = 25;
+document.getElementById("start-demo-3").innerHTML = myFirstVar;
+
+function changeParagraph () {
+    document.getElementById("start-demo-2").innerHTML ="Ten paragraf uległ zmianie po wywołaniu skryptu";
+}
+
+
