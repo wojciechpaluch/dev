e06cebab7eb198d816171863414b36a61698b4ce
Wojciech Paluch <wojciech.paluch@o2.pl>
2017-08-15 14:12:13 +0200

    Added comment and new functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Funkcja odpowiadająca za zmianę paragrafu
 function changeParagraph() {
     document.getElementById("start-demo-2").innerHTML = "Ten paragraf uległ zmianie po wywołaniu skryptu";
 }
@@ -8,18 +9,23 @@ var myFirstVar = 1;
 
 }
 
+
+// Funkcja wyświetlająca tablicę od drugiej elementu
+
 var table = ["User", "User2", "User3"];
 
 {
 for (var i=1; i<table.length; i++)
 
-    document.getElementById("start-demo-4").innerHTML = table; }
+    document.getElementById("start-demo-4").innerHTML += table[i]; }
 
 var table2 = [1,2,3,4,5,6,7,8,9,10];
 {
     for (var i = 1; i < 10; i++)
-    document.getElementById("start-demo-5").innerHTML = table2; }
+    document.getElementById("start-demo-5").innerHTML += table2[i]; }
 
+
+// Funkcja obliczająca ilość znaków w tekscie
 
 var text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 document.getElementById("start-demo-6").innerHTML = text.length + " << wyświetla ilość liter ";
@@ -35,3 +41,9 @@ var person = {
 
 document.getElementById("start-demo-7").innerHTML = person.firstName + " " + person.lastName + " from " + person.born + " have " + person.age + " years old ";
 
+(function() {
+    var currentDate = new Date();
+    var year = currentDate.getFullYear();
+    document.getElementById("start-demo-8").innerHTML = "Dzisiaj jest " + year + " rok ";
+
+})();
